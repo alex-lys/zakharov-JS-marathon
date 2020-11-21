0eb1d7fe9fbcc91e6f45c809aa29d01986c04612
Alex <la.prof@ya.ru>
2020-11-22 01:44:18 +0300

    HomeWork-3

--- PokemonJS/main.js
+++ PokemonJS/main.js
@@ -1,75 +1,106 @@
-// const $bntKick = document.getElementById('btn-kick');
-const $control = document.querySelector('.control');
+function $getElemById(id) {
+  return document.getElementById(id);
+}
+
+const $control = $getElemById('control');
+const $logs = $getElemById('logs');
 
 const character = {
   name: 'Picachu',
   defaultHP: 100,
   damageHP: 100,
-  elHP: document.getElementById('health-character'),
-  elProgressbar: document.getElementById('progressbar-character'),
+  elHP: $getElemById('health-character'),
+  elProgressbar: $getElemById('progressbar-character'),
+  elKick: $getElemById('btn-kick-character'),
+  changeHP: changeHP,
+  renderHP: renderHP,
+  renderHPLife: renderHPLife,
+  renderProgressbar: renderProgressbar,
 };
 
 const enemy = {
   name: 'Charmander',
-  defaultHP: 100,
-  damageHP: 100,
-  elHP: document.getElementById('health-enemy'),
-  elProgressbar: document.getElementById('progressbar-enemy'),
+  defaultHP: 200,
+  damageHP: 200,
+  elHP: $getElemById('health-enemy'),
+  elProgressbar: $getElemById('progressbar-enemy'),
+  elKick: $getElemById('btn-kick-enemy'),
+  changeHP: changeHP,
+  renderHP: renderHP,
+  renderHPLife: renderHPLife,
+  renderProgressbar: renderProgressbar,
 };
 
-function renderHPLife(person) {
-  console.log(person.elHP.innerText);
-
-  person.elHP.innerText = person.damageHP + '/' + person.defaultHP;
+function renderHPLife() {
+  this.elHP.innerText = `${this.damageHP}/${this.defaultHP}`;
 }
 
-function renderProgressbar(person) {
-  person.elProgressbar.style.width = person.damageHP + '%';
+function renderProgressbar() {
+  this.elProgressbar.style.width = `${this.damageHP / (this.defaultHP / 100)}%`;
 }
 
-function renderHP(person) {
-  renderHPLife(person);
-  renderProgressbar(person);
+function renderHP() {
+  this.renderHPLife();
+  this.renderProgressbar();
 }
 
-function changeHP(count, person) {
-  if (person.damageHP < count) {
-    person.damageHP = 0;
-    console.log('Покемон ' + person.name + ' проиграл');
-    $bntKick.disabled = 'true';
-  } else {
-    person.damageHP -= count;
+function changeHP(count) {
+  this.damageHP -= count;
+
+  const log =
+    this === enemy
+      ? generateLog(this, character, count)
+      : generateLog(this, enemy, count);
+
+  $logs.insertBefore(log, $logs.children[0]);
+  console.log(log);
+
+  if (this.damageHP <= 0) {
+    this.damageHP = 0;
+    console.log(`Покемон '${this.name}' проиграл`);
+    this.elKick.disabled = 'true';
   }
 
-  renderHP(person);
+  this.renderHP();
 }
 
 function random(num) {
   return Math.ceil(Math.random() * num);
 }
 
+function generateLog(firstPerson, secondPerson, damage) {
+  const logs = [
+    `${firstPerson.name} вспомнил что-то важное, но неожиданно ${secondPerson.name}, не помня себя от испуга, ударил в предплечье врага. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} поперхнулся, и за это ${secondPerson.name} с испугу приложил прямой удар коленом в лоб врага. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} забылся, но в это время наглый ${secondPerson.name}, приняв волевое решение, неслышно подойдя сзади, ударил. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} пришел в себя, но неожиданно ${secondPerson.name} случайно нанес мощнейший удар. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} поперхнулся, но в это время ${secondPerson.name} нехотя раздробил кулаком <вырезанно цензурой> противника. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} удивился, а ${secondPerson.name} пошатнувшись влепил подлый удар. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} высморкался, но неожиданно ${secondPerson.name} провел дробящий удар. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} пошатнулся, и внезапно наглый ${secondPerson.name} беспричинно ударил в ногу противника. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} расстроился, как вдруг, неожиданно ${secondPerson.name} случайно влепил стопой в живот соперника. -${damage} [${firstPerson.damageHP}/100]`,
+    `${firstPerson.name} пытался что-то сказать, но вдруг, неожиданно ${secondPerson.name} со скуки, разбил бровь сопернику. -${damage} [${firstPerson.damageHP}/100]`,
+  ];
+
+  const $p = document.createElement('p');
+  $p.innerText = logs[random(logs.length - 1)];
+
+  return $p;
+}
+
 function init() {
   console.log('Start Game!');
 
-  renderHP(character);
-  renderHP(enemy);
+  character.renderHP();
+  enemy.renderHP();
 }
 
 init();
 
-// $bntKick.addEventListener('click', function () {
-//   console.log('kick');
-
-//   changeHP(random(20), character);
-//   changeHP(random(20), enemy);
-// });
-
 $control.addEventListener('click', function (event) {
-  console.log('kick');
-
   if (event.target.id === 'btn-kick-character') {
-    changeHP(random(20), character);
+    character.changeHP(random(20));
   } else if (event.target.id === 'btn-kick-enemy') {
-    changeHP(random(20), enemy);
+    enemy.changeHP(random(20));
   }
 });
